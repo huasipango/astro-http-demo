7b28454028ebfe63939f3945941bb173bc3cd9e0
Anthony Cabrera <anthony.cabrera@minu.mx>
2025-03-16 13:15:13 +0100

    Update dependencies and configure Cloudflare adapter in Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  output: "static",
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'server',
+  adapter: cloudflare(),
 });
